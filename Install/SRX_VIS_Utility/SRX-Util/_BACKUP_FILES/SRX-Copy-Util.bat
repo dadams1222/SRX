75bcd571e841736b85f0ec57405fde9f031a251e
dadams1222 <67166054+dadams1222@users.noreply.github.com>
2024-02-08 15:33:11 -0800

    import

---
+++ Install/SRX_VIS_Utility/SRX-Util/_BACKUP_FILES/SRX-Copy-Util.bat
@@ -0,0 +1,3 @@
+@echo off 
+cd C:\Program Files (x86)\SRX-Copy-Util
+SRX-Copy-Util.exe "varicella_English_08092021.pdf|19D18724-D465-4039-A6A0-6EF989695304.pdf"
---
+++ Install/SRX_VIS_Utility/SRX-Util/srxcpy.bat
@@ -0,0 +1,3 @@
+@echo off 
+cd C:\Program Files (x86)\SRX-Copy-Util
+SRX-Copy-Util.exe "varicella_English_08092021.pdf|50F61599-4628-46E2-A6EA-96D43605EB0B.pdf"
